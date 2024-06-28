cf4445bf11f55436c1ff6a8bc8f72ee93513c86d
Tahmid Ferdous <Tahmid>
2024-06-28 21:52:47 +0600

    learnt npm server and react useEffect, wrapping up at 1:52:00

--- Project/react-crash-2024/src/components/JobListings.jsx
+++ Project/react-crash-2024/src/components/JobListings.jsx
@@ -1,9 +1,29 @@
 import React from "react";
-import jobs from "../jobs.json";
+// import jobs from "../jobs.json";
 import JobListing from "./JobListing";
 
+import { useState, useEffect } from "react";
+
 const JobListings = ({ isHome = false }) => {
-  const recentJobs = isHome ? jobs : jobs.slice(0, 3);
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch("http://localhost:8000/jobs");
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log("Error fetching data");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
+
   return (
     <>
       <section className="bg-blue-50 px-4 py-10">
@@ -12,7 +32,7 @@ const JobListings = ({ isHome = false }) => {
             {isHome ? "Recent Jobs" : "Browse Jobs"}
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {recentJobs.map((job, key) => (
+            {jobs.slice(0, 3).map((job, key) => (
               <JobListing job={job} key={key} />
             ))}
           </div>
